d2e72012f100c148844425f182f874eee4c3e09e
Mark Arenz <arenz.mark@gmail.com>
2024-10-26 19:39:30 -0400

    feat: TQ2 API Setup

---
+++ api/src/index.mjs
@@ -0,0 +1,37 @@
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
+import { createItem, getItems, updateItem } from "./utils.mjs";
+
+const client = new DynamoDBClient({});
+const dynamoDb = DynamoDBDocumentClient.from(client);
+
+export const handler = async (event, context) => {
+  try {
+    const ip = event?.requestContext?.http?.sourceIp ?? null;
+    const method = event?.requestContext?.http?.method ?? null;
+    const path = event?.requestContext?.http?.path.replace("/", "") ?? null;
+    const body = event?.body ? JSON.parse(event?.body) : null;
+    const searchParams = event?.queryStringParameters ?? {};
+
+    const functionMap = {
+      GET: getItems,
+      POST: createItem,
+      PUT: updateItem,
+    };
+
+    const params = { method, path, searchParams, ip, dynamoDb, body };
+
+    if (functionMap[method]) {
+      return functionMap[method](params);
+    }
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ error: "Invalid method or path prodvided" }),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error }),
+    };
+  }
+};
---
+++ api/src/utils.mjs
@@ -0,0 +1,127 @@
+import { randomBytes } from "crypto";
+import { PutCommand, GetCommand } from "@aws-sdk/lib-dynamodb";
+import { ScanCommand } from "@aws-sdk/client-dynamodb";
+
+const fieldNames = {
+  games: ["id", "userId", "userName", "name", "description"],
+  users: ["id", "name"],
+};
+
+const getBodyData = ({ body, path }) => {
+  const data = {};
+  fieldNames[path].forEach((fieldName) => {
+    console.log("CHECKING...", fieldName, body[fieldName]);
+    data[fieldName] = body[fieldName] ?? null;
+  });
+  return data;
+};
+
+const tableNames = {
+  games: "prod-tq-games",
+  users: "prod-tq-users",
+};
+
+const toUrlString = (buffer) => {
+  return buffer
+    .toString("base64")
+    .replace(/\+/g, "-")
+    .replace(/\//g, "_")
+    .replace(/=/g, "");
+};
+
+export const createItem = async (params) => {
+  const { path, dynamoDb, body } = params;
+  const { id: rawId } = body;
+
+  const bodyData = getBodyData({ body, path });
+  // For Users, we pull in the sub ID from authentication
+  // Otherwise, we create a new ID
+  const id = rawId ?? toUrlString(randomBytes(32));
+
+  const command = new PutCommand({
+    TableName: tableNames[path],
+    Item: {
+      ...bodyData,
+      id,
+      dateCreated: new Date().toISOString(),
+      dateUpdated: new Date().toISOString(),
+    },
+  });
+  const resp = await dynamoDb.send(command);
+  const success = resp["$metadata"].httpStatusCode === 200;
+  return {
+    statusCode: success ? 201 : 500,
+    body: JSON.stringify({
+      message: success ? "Item created successfully." : "Error creating item",
+      id,
+      resp: resp["$metadata"].httpStatusCode,
+    }),
+  };
+};
+
+export const updateItem = async (params) => {
+  const { path, dynamoDb, body } = params;
+  const { id } = body;
+  const bodyData = getBodyData({ body, path });
+
+  const command = new PutCommand({
+    TableName: tableNames[path],
+    Item: {
+      ...bodyData,
+      dateUpdated: new Date().toISOString(),
+    },
+  });
+  const resp = await dynamoDb.send(command);
+  const success = resp["$metadata"].httpStatusCode === 200;
+  return {
+    statusCode: success ? 201 : 500,
+    body: JSON.stringify({
+      path,
+      message: success ? "Item updated successfully." : "Error updating item",
+      id,
+      resp: resp["$metadata"].httpStatusCode,
+    }),
+  };
+};
+
+export const getItems = async (params) => {
+  const { path, dynamoDb, searchParams } = params;
+
+  const id = searchParams?.id ?? null;
+  if (id) {
+    const command = new GetCommand({
+      TableName: tableNames[path],
+      Key: {
+        id,
+      },
+    });
+    const resp = await dynamoDb.send(command);
+    const item = resp?.Item ?? null;
+
+    if (item) {
+      return {
+        statusCode: 200,
+        body: JSON.stringify({ success: true, item }),
+      };
+    }
+  }
+
+  // TODO: Load more pagination
+  const command = new ScanCommand({
+    TableName: tableNames[path],
+  });
+  const resp = await dynamoDb.send(command);
+  const items = resp?.Items ?? null;
+
+  if (items) {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ success: true, items }),
+    };
+  }
+
+  return {
+    statusCode: 404,
+    body: JSON.stringify({ success: false }),
+  };
+};
