fbb971201bcba1c7c59d9fc36241e3b287667e65
Mark Arenz <arenz.mark@gmail.com>
2025-06-09 19:18:11 -0400

    feat:TQ20 fix max-height on common modal

--- src/app/features/main/ui/components/common-modal/common-modal.component.ts
+++ src/app/features/main/ui/components/common-modal/common-modal.component.ts
@@ -1,4 +1,11 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import {
+  Component,
+  Input,
+  Output,
+  EventEmitter,
+  ElementRef,
+  ViewChild,
+} from '@angular/core';
 import { ModalBgComponent } from '@main/ui/components/modal-bg/modal-bg.component';
 import { ButtonComponent } from '@app/features/main/ui/components/button/button.component';
 
@@ -17,6 +24,9 @@ export class CommonModalComponent {
   @Output() onClose = new EventEmitter();
   @Output() onConfirm = new EventEmitter();
   @Output() onCancel? = new EventEmitter();
+  @ViewChild('elementRefHeader') elementRefHeader!: ElementRef;
+  @ViewChild('elementRefFooter') elementRefFooter!: ElementRef;
+  @ViewChild('elementRefContent') elementRefContent!: ElementRef;
 
   handleConfirmClick() {
     this.onConfirm.emit();
@@ -28,5 +38,14 @@ export class CommonModalComponent {
     if (this.onCancel) {
       this.onCancel.emit();
     }
+  }
+  ngAfterViewInit() {
+    const h1 = this.elementRefHeader.nativeElement.offsetHeight;
+    const h2 = this.elementRefFooter.nativeElement.offsetHeight;
+    const commonModalHeaderFooterHeight = h1 + h2 + 20;
+    this.elementRefContent.nativeElement.style.setProperty(
+      'max-height',
+      `calc(100vh - ${commonModalHeaderFooterHeight}px)`
+    );
   }
 }
